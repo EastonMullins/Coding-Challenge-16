f864536131dfaae7eb9a240fab23d6d637a3d474
Easton <eastonmullins@usf.edu>
2025-03-30 11:11:43 -0400

    Task 2 - Implement fetchProductsThen using .then() and .catch()

--- cc_16.js
+++ cc_16.js
@@ -1,2 +1,14 @@
 //Task 1 - Set up the project structure
 //Completed in html
+
+//Task 2 - Implement fetchProductsThen using .then() and .catch()
+
+function fetchProductsThen() {
+    fetch("https://www.course-api.com/javascript-store-products")
+    .then(response => response.json())
+    .then(products => {
+        console.log("Product Name: ");
+        products.forEach(product => console.log(product.fields.name));
+    })
+    .catch(error => handleError(error));
+}
